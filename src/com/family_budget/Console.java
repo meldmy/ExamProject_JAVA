760c4afd8ac4b9c2716124f989d9ac2b1855b90f
Dmytro Melnychuk <melnichukdv@gmail.com>
2016-03-19 12:59:56 +0100

    First commit

---
+++ src/com/family_budget/Console.java
@@ -0,0 +1,10 @@
+package com.family_budget;
+
+/**
+ * Created by Dmytro Melnychuk on 19.03.16.
+ */
+public class Console {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
